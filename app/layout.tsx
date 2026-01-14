a3b14ff08a2dcc84c0e3966780cde00e676c561f
thashibtk <thashibtk@gmail.com>
2026-01-14 12:59:02 +0530

    Analytics update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Preloader from "@/components/Preloader";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"], variable: '--font-inter' });
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"], variable: '--font-dm-sans' });
