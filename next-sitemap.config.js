4bda436fd432d7f39f272a24fb9bdff665bc2097
thashibtk <thashibtk@gmail.com>
2026-01-26 17:15:51 +0530

    xml update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,6 @@ module.exports = {
   siteUrl: "https://www.siztec.com",  // change to your canonical domain
   generateRobotsTxt: false,
   sitemapSize: 5000,
+  generateIndexSitemap: false,
   exclude: ["/admin/*"], // optional
 };
